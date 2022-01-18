1c4ad8854bb7c670b2319c6bfc201c28c24149e2
enfel <enfel.mhamedi@yahoo.fr>
2022-01-18 14:57:31 +0100

    quest 05 route challenge

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,11 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/program", name="program_")
+ */
 
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/program/", name="program_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
@@ -18,7 +21,11 @@ class ProgramController extends AbstractController
             'website' => 'Wild Séries',
         ]);
     }
-}
-{
-
+    /**
+     * @Route("/{id<\d+>}", methods={"GET"}, name="show")
+     */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id]);
+    }
 }
